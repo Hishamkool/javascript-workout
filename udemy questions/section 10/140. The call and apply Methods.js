8eb72283a798f425a76f731d4f0e5eeb3a7c7eb2
Hisham <linkedin.hisham@gmail.com>
2025-12-10 15:28:50 +0530

    challenge 1, bind method , call and apply methods.

--- udemy questions/section 10/140. The call and apply Methods.js
+++ udemy questions/section 10/140. The call and apply Methods.js
@@ -1,7 +1,7 @@
 console.log(`\n------------CALL METHOD------------`);
 
 console.log(`\n------------AIR INDIA------------`);
-const airIndia = {
+export const airIndia = {
   airline: "AirIndia",
   iataCode: "AI",
   bookings: [],
@@ -22,7 +22,7 @@ flight ${this.airline}-${this.iataCode} with a flight number ${flightNumber}, Ha
   },
 };
 
-const generateFlightNo = function () {
+export const generateFlightNo = function () {
   //converts the random number to base 36 which includes numbers 0-9 and a-z like 0.572398465 → "0.x9b3q" now we use substring to cut the 0 and the decimal and start from the 2nd index to 5th index to include only 3 numbers
   const letters = Math.random().toString(36).substring(2, 5).toUpperCase();
   const random = Math.floor(1000 + Math.random() * 999);
@@ -36,14 +36,14 @@ console.log(airIndia.bookings);
 console.log(`\n------------QATAR AIRWAYS------------`);
 
 //ANOTHER FLIGHT COMPANY NEEDS THE SAME BOOKING FORMAT
-const qatarAirways = {
+export const qatarAirways = {
   airline: "QATAR AIRWAYS",
   iataCode: "QTA",
   bookings: [],
 };
 
 //storing the ariindia booking function to a variable
-const book = airIndia.bookFlight;
+export const book = airIndia.bookFlight;
 
 // now we will pass [this] keyword to qatar airways to book QATAIRWAYS FLIGHT
 book.call(qatarAirways, generateFlightNo(), "Sahul hameed");
@@ -58,7 +58,7 @@ console.log("Bookings qta", qatarAirways.bookings);
 console.log(`\n------------INDIGO FLIGHT------------`);
 
 // indigo flight
-const indigoAirways = {
+export const indigoAirways = {
   airline: "Indigo Airlines",
   iataCode: "IA",
   bookings: [],
@@ -81,3 +81,92 @@ book.apply(qatarAirways, bookingArray);
 //same thing can be done in call method using the spread operator
 console.log("same thing can be done by spread operator in call method");
 book.call(airIndia, ...bookingArray);
+
+console.log(`\n------------BIND METHOD------------`);
+
+// NOW THIS VARIABLE IS ALWAYS BINDED TO QATAR AIRWAYS
+const bookFromQATAR_AIRWAYS = book.bind(qatarAirways);
+bookFromQATAR_AIRWAYS(generateFlightNo(), "Abdul Razak");
+//                    flight number ,     name
+
+console.log(`\n------------binding parameters------------`);
+
+const bookqat123 = book.bind(qatarAirways, "123");
+// this binds the quatar airways with flight number always being 123
+console.log("all in same flight");
+
+bookqat123("muhammed hisham");
+bookqat123("Sabira");
+bookqat123("muhammed shameem");
+
+//partial application of bind
+console.log(`\n------------APPLICATION OF BIND------------`);
+console.log(`Using the same method to perform different task`);
+
+const compnayOne = {
+  company: "Hisham Pvt LMT",
+};
+
+const compnayTwo = {
+  company: "Adani Group",
+};
+const commonMultiplier = function (name, rate, value) {
+  const greetings = "Good Morning";
+  const result = value + value * rate;
+  console.log(
+    `${greetings}, the ${name} is ${result} for the value ${value} of the company ${this.company}`
+  );
+  return result;
+};
+/*
+ when the common multiplier function didnt have a this.company value 
+
+// here we are passing null because there is not this value in the common multiplier
+const addGST = commonMultiplier.bind(null, "GST", 0.18);
+addGST(399);
+addGST(187);
+addGST(88);
+addGST(788);
+
+*/
+
+// here we are passing null because there is not this value
+console.log(`\n------------hisham group------------`);
+
+const addGST = commonMultiplier.bind(compnayOne, "GST", 0.18);
+addGST(187);
+addGST(88);
+addGST(788);
+addGST(399);
+
+console.log(`\n------------adani group------------`);
+
+const addGSTADANI = commonMultiplier.bind(compnayTwo, "GST", 300);
+addGSTADANI(399);
+
+console.log(
+  `\n------------add vat using same method for gst for hisham group------------`
+);
+
+// we need a new function called add vat which has the same format as addGst except that its rate is 0.23 always
+const addVat = commonMultiplier.bind(compnayOne, "VAT", 0.23);
+addVat(50);
+addVat(150);
+addVat(230);
+addVat(23);
+
+console.log(
+  `\n------------function that returns a function that does the same------------`
+);
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const gst = addTaxRate(0.18)(399);
+console.log(`Gst is ${gst} for the value 399`);
+
+const vat = addTaxRate(0.23)(399);
+console.log(`Vat is ${vat} for the value 50`);
